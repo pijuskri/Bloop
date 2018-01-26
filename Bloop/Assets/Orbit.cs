bfe6002bd3dcfc3b16e90865d9a8db1519c540e4
pijuskri <pijuskri@gmail.com>
2018-01-27 01:50:34 +0200

    placement improvments

---
+++ Bloop/Assets/Orbit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+    public Vector2 direction = new Vector2(0,0);
+    public GameObject planet;
+    float speed=0.2f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(planet.transform.position, direction, speed);
+        //transform.rotation = Quaternion.Euler(0, 0, 0);
+        transform.LookAt(planet.transform);
+    }
+}
--- Bloop/Assets/Place.cs
+++ Bloop/Assets/Place.cs
@@ -6,6 +6,8 @@ public class Place : MonoBehaviour {
 
     public GameObject sattelite;
     public GameObject LinePrefab;
+    GameObject Line;
+    public GameObject planet;
     Vector3 LineStart = new Vector3();
 	// Use this for initialization
 	void Start () {
@@ -30,20 +32,46 @@ public class Place : MonoBehaviour {
                 Vector3 temp = Input.mousePosition;
                 
                 temp = hitInfo.point;
-               // temp.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
-                Instantiate(sattelite, temp, new Quaternion());
+                LineStart = hitInfo.point;
+                // temp.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+                //Instantiate(sattelite, temp, new Quaternion());
+
+                Line = Instantiate(LinePrefab, new Vector3(0, 0, 0), new Quaternion());
+                Line.GetComponent<LineRenderer>().SetPosition(0, LineStart);
             }
         }
-    }
-    private void OnMouseDown()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        RaycastHit hitInfo;
-        if (Physics.Raycast(ray, out hitInfo))
+        if (Input.GetButton("Fire1"))
         {
-            LineStart = hitInfo.point;
-            // temp.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                Vector3 temp = Input.mousePosition;
+                temp = hitInfo.point;
+                // temp.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+                Line.GetComponent<LineRenderer>().SetPosition(1, temp);
+            }
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            GameObject temp = Instantiate(sattelite, LineStart, new Quaternion());
+            Vector2 direction = new Vector2();
+            
+            LineStart = Camera.main.WorldToScreenPoint(LineStart);
+            Vector3 LineEnd = Camera.main.WorldToScreenPoint(Line.GetComponent<LineRenderer>().GetPosition(1));
+
+            direction = new Vector2( LineEnd.x - LineStart.x, LineEnd.y - LineStart.y);
+            if (direction.x > direction.y) direction = direction / direction.x;
+            else if (direction.x < direction.y) direction = direction / direction.y;
+            direction.x *= -1;
+
+            //print(LineStart + " " + LineEnd);
+            direction = new Vector2(direction.y, direction.x);
+            temp.GetComponent<Orbit>().direction = direction;
+            print(direction);
+            temp.GetComponent<Orbit>().planet = planet;
+            Destroy(Line);
         }
     }
 }
