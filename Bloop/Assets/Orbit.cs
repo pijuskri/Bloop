c79add5ae5a67617b2b95f2cafbd09fcb4c1c94e
pijuskri <pijuskri@gmail.com>
2018-01-27 05:15:50 +0200

    i hate my life

--- Bloop/Assets/Orbit.cs
+++ Bloop/Assets/Orbit.cs
@@ -15,19 +15,18 @@ public class Orbit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //transform.RotateAround(planet.transform.position, new Vector3(direction.x, 0, 0), speed);
-        //transform.rotation = Quaternion.Euler(0, 0, 0);
         transform.LookAt(planet.transform);
         transform.Translate(direction * speed);
-        //print(transform.rotation.eulerAngles);
         if ((transform.rotation.eulerAngles.x > 89.7 && transform.rotation.eulerAngles.x < 90.3)||(transform.rotation.eulerAngles.x > 269.7 && transform.rotation.eulerAngles.x < 270.4))
         {
-            transform.Translate(direction * speed * 3);
+            transform.Translate(direction * speed * 10);
             direction *= -1;
         }
         if(Vector3.Distance(planet.transform.position, transform.position)> initialDistance) transform.Translate(Vector3.forward * 0.02f);
 
         if (direction.x == 0 && direction.y == 0) Destroy(gameObject);
+
+
     }
 
 }
